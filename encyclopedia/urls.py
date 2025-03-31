4ea5a18321114a3e91fc81342f75e7b9df072703
cordelia-meq <cordelia@marketseq.ai>
2025-03-31 11:30:56 -0400

    update gitignore

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.entry, name="title")
+    path("search", views.search, name="search"),
+    path("<str:title>", views.entry, name="title"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,21 +2,16 @@ from django.shortcuts import render
 from django import forms
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-
 from . import util
 
-class newSearchForm(forms.Form):
-    search = forms.CharField(label="Search")
+
 def index(request):
-    if request.method == "POST":
-        request.session["search"] = []
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
 def entry(request, title):
     entry = util.get_entry(title) 
-    print(entry)
     if entry is None: 
         return render(request, "encyclopedia/error.html")
     else: 
@@ -24,3 +19,16 @@ def entry(request, title):
         "title": title,
         "entry": util.get_entry(title)
     })
+
+def search(request):
+    if request.method == "GET":
+        search_term = request.GET.get("q")
+        print(search_term)
+        entry = util.get_entry(search_term) 
+        if entry is None: 
+            return render(request, "encyclopedia/error.html")
+        else: 
+            return render(request, "encyclopedia/entry.html", {
+            "title": search_term,
+            "entry": util.get_entry(search_term)
+        })
